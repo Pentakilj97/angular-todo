ad3b8ea7cacfcae19c99f60637e6d57db36cdea2
crex02 <127298119+Pentakilj97@users.noreply.github.com>
2023-05-15 12:02:23 +0200

    add todo

---
+++ src/app/components/todo-add/todo-add.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Todo } from 'src/app/models/todo';
+
+@Component({
+  selector: 'app-todo-add',
+  templateUrl: './todo-add.component.html',
+  styleUrls: ['./todo-add.component.scss']
+})
+export class TodoAddComponent {
+
+  newTodo: Todo={title:'',priority:1, description:''}
+  @Output() todoCreated:EventEmitter<Todo> = new EventEmitter()
+
+  saveTodo(){
+    // const titleInput =document.getElementById('input-title') as HTMLInputElement
+    // const titleDescripitonInput=document.getElementById('input-description') as HTMLInputElement
+    // const priorityInput =document.getElementById('input-priority') as HTMLInputElement
+
+    // const newTodo: Todo={title: titleInput.value,
+    //                      description:titleDescripitonInput.value,
+    //                      priority:parseInt(priorityInput.value)}
+    this.todoCreated.emit({...this.newTodo})
+  }
+}
+
+
---
+++ src/app/components/todo-list/todo-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+import { Todo } from 'src/app/models/todo';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.scss']
+})
+export class TodoListComponent {
+  @Input() todoArray:Todo[] =[]
+
+  deleteTodo(todoToDelete:Todo){
+    console.log('devo cancellare', todoToDelete.title)
+    this.todoArray= this.todoArray.filter(todo =>todo.title !==todoToDelete.title)
+  }
+}
